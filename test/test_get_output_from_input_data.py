8c43fe53a5cceb6aab470004fe3c4ba008092f66
milocortes <hermilocg@hotmail.com>
2023-02-16 12:56:28 -0600

    add get_output_input

---
+++ test/test_get_output_from_input_data.py
@@ -0,0 +1,29 @@
+import os
+
+os.environ['LAC_PATH'] = '/home/milo/Documents/julia/lac_decarbonization'
+
+from sisepuede_calibration.calibration_lac import ModelOutputData
+
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/egobiernoytp/lac_decarbonization/main/ref/fake_data/fake_data_complete.csv")
+
+## Test AFOLU
+ModelOutputData.get_output_input_data_AFOLU(df, False)
+
+## Test CircularEconomy
+ModelOutputData.get_output_input_data_CircularEconomy(df, False)
+ModelOutputData.get_output_input_data_CircularEconomy(df, True)
+
+## Test IPPU
+ModelOutputData.get_output_input_data_IPPU(df, False)
+ModelOutputData.get_output_input_data_IPPU(df, True)
+
+## Test NonElectricEnergy
+ModelOutputData.get_output_input_data_NonElectricEnergy(df, True)
+
+## Test ElectricEnergy
+ModelOutputData.get_output_input_data_ElectricEnergy(df, True)
+
+## Test AllEnergy
+ModelOutputData.get_output_input_data_AllEnergy(df, True)
